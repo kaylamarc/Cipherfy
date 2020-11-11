408d35dbbfc1389a4432d28d7715b69387e02a92
Kayla Marcantonio <kkcoons@aol.com>
2020-11-10 22:13:43 -0500

    Updated documentation

--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -1,25 +1,36 @@
 public class CaesarCipher {
 
+	/**
+	 * alphabet to use for indexing
+	 */
 	private final String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	
-	private int shift;
+	private int shift; // shift value
 	
 	public CaesarCipher(int shift) {
 		this.shift = shift;
 	}
 	
+	/**
+	 * encrypts plaintext
+	 * @param plaintext
+	 * @return
+	 */
 	public StringBuffer encrypt(String plaintext) {
-
+		
+		// ensure uniform ASCII values
 		plaintext = plaintext.toUpperCase();
 
 		StringBuffer ciphertext = new StringBuffer();
 
+		// go through each index of plaintext and shift
 		for (int i = 0; i < plaintext.length(); i++) {
 			// get the value (index) of each character in the alphabet
 			int alphaVal = alphabet.indexOf(plaintext.charAt(i));
 
 			int enVal = (shift + alphaVal) % 26;
-
+			
+			// get shifted & encrypted character 
 			char ciphVal = alphabet.charAt(enVal);
 
 			// add the value to the cipher text
@@ -29,11 +40,19 @@ public class CaesarCipher {
 		return ciphertext;
 	}
 
+	/**
+	 * decrypts ciphertext
+	 * @param ciphertext
+	 * @return
+	 */
 	public StringBuffer decrypt(String ciphertext) {
+		
+		// ensure uniform ASCII values
 		ciphertext = ciphertext.toUpperCase();
 
 		StringBuffer plaintext = new StringBuffer();
 
+		// go through each index of ciphertext and shift
 		for (int i = 0; i < ciphertext.length(); i++) {
 			int alphaVal = alphabet.indexOf(ciphertext.charAt(i));
 
@@ -44,6 +63,7 @@ public class CaesarCipher {
 				decrVal += alphabet.length();
 			}
 			
+			// get decrypted character
 			char plainVal = alphabet.charAt(decrVal);
 			
 			plaintext.append(plainVal);
